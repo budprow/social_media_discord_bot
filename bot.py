186687233f9a80bbcd3eb8450a6fc676c617fcf5
Bud Prow <radjams@gmail.com>
2025-04-27 23:38:36 -0400

    created bot.py and connects to discord

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+TOKEN = ''
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print(f'Bot ID: {bot.user.id}')
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong!')
+
+bot.run(TOKEN)
